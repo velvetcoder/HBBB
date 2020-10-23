7f1f9c474c2ba2735a10efc19867abcd05dea32c
velvetcoder <velvetcoder@users.noreply.github.com>
2020-10-22 23:49:51 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -132,99 +132,100 @@ function siiPhraseOpen() {
 var mM1 = "BLACK CAT"
 var mM2 = "SPEAKERS"
 
-function B1() {
+function on() {
     document.getElementById("overlay").style.display = "block";
-  }
-  
-  function A1() {
-    document.getElementById("overlay").style.display = "none";
-  }
+}
 
-  function B2() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function A2() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function off() {
+document.getElementById("overlay").style.display = "none";
+}
 
-  function on3() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off3() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function on2() {
+document.getElementById("overlay2").style.display = "block";
+}
 
-  function on4() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off4() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function off2() {
+document.getElementById("overlay2").style.display = "none";
+}
 
-  function on5() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off5() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function on3() {
+document.getElementById("overlay3").style.display = "block";
+}
 
-  function on6() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off6() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function off3() {
+document.getElementById("overlay3").style.display = "none";
+}
 
-  function on7() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off7() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function on4() {
+document.getElementById("overlay4").style.display = "block";
+}
 
-  function on8() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off8() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function off4() {
+document.getElementById("overlay4").style.display = "none";
+}
 
-  function on9() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off9() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function on5() {
+document.getElementById("overlay5").style.display = "block";
+}
 
-  function on10() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off10() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function off5() {
+document.getElementById("overlay5").style.display = "none";
+}
 
-  function on11() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off11() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function on6() {
+document.getElementById("overlay6").style.display = "block";
+}
 
-  function on12() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off12() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function off6() {
+document.getElementById("overlay6").style.display = "none";
+}
 
+function on7() {
+document.getElementById("overlay7").style.display = "block";
+}
+
+function off7() {
+document.getElementById("overlay7").style.display = "none";
+}
+
+function on8() {
+document.getElementById("overlay8").style.display = "block";
+}
+
+function off8() {
+document.getElementById("overlay8").style.display = "none";
+}
+
+function on9() {
+document.getElementById("overlay9").style.display = "block";
+}
+
+function off9() {
+document.getElementById("overlay9").style.display = "none";
+}
+
+function on10() {
+document.getElementById("overlay10").style.display = "block";
+}
+
+function off10() {
+document.getElementById("overlay10").style.display = "none";
+}
+
+function on11() {
+document.getElementById("overlay11").style.display = "block";
+}
+
+function off11() {
+document.getElementById("overlay11").style.display = "none";
+}
+
+function on12() {
+document.getElementById("overlay12").style.display = "block";
+}
+
+function off12() {
+document.getElementById("overlay12").style.display = "none";
+}
+
+ 
